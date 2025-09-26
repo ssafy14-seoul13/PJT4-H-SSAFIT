3a6526e5a6cbe516f83e3ca7e78ec8fdb461c9ad
echo98100 <echo98100@gmail.com>
2025-09-26 17:50:45 +0900

    feature : Video 수정

--- PJT4-H-SSAFIT/src/main/java/com/ssaft/serviceImpl/VideoServiceImpl.java
+++ PJT4-H-SSAFIT/src/main/java/com/ssaft/serviceImpl/VideoServiceImpl.java
@@ -1,5 +1,60 @@
 package com.ssaft.serviceImpl;
 
-public class VideoServiceImpl {
+import java.util.List;
+
+import com.ssafit.Dao.VideoDao;
+import com.ssafit.DaoImpl.VideoDaoImpl;
+import com.ssafit.service.VideoService;
+import com.ssaft.Dto.Video;
+
+public class VideoServiceImpl implements VideoService {
+	
+	
+	// 싱글턴으로 구현
+	private static VideoService service = new VideoServiceImpl();
+	
+	private VideoServiceImpl() {
+	}
+	
+	public static VideoService getInstance() {
+		return service;
+	}
+	
+	private VideoDao dao = VideoDaoImpl.getInstance();
+	
+
+	@Override
+	public void ins(Video video) {
+		
+		dao.insert(video);
+	}
+
+	@Override
+	public Video selDtl(int id) {
+
+		return dao.select(id);
+	}
+
+	@Override
+	public void updViewCnt(int id) {
+
+		dao.updateViewCount(id);
+	}
+
+	@Override
+	public List<Video> selList() {
+		return dao.selectAll();
+	}
+
+	@Override
+	public void del(int id) {
+		dao.delete(id);
+	}
+
+	@Override
+	public void upd(Video video) {
+		
+		dao.update(video);
+	}
 
 }
