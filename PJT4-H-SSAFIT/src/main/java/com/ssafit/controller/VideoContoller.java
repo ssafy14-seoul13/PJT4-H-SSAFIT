806358aa3a9d7790fa781ef1a4cf76db3f2955c0
echo98100 <echo98100@gmail.com>
2025-09-26 18:28:48 +0900

    VideoController 수정

--- PJT4-H-SSAFIT/src/main/java/com/ssafit/controller/VideoContoller.java
+++ PJT4-H-SSAFIT/src/main/java/com/ssafit/controller/VideoContoller.java
@@ -3,6 +3,7 @@ package com.ssafit.controller;
 import java.io.IOException;
 
 import com.ssafit.service.VideoService;
+import com.ssaft.Dto.Video;
 import com.ssaft.serviceImpl.VideoServiceImpl;
 
 import jakarta.servlet.ServletException;
@@ -33,6 +34,85 @@ public class VideoContoller extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+		
+		switch (action) {
+		case "selectVideoList":
+			selectVideoList(request, response);
+			break;
+		
+		case "selectVideoDetail":
+			selectVideoDetail(request, response);
+			break;
+			
+		case "insertVideo":
+			insertVideo(request, response);
+			break;
+			
+		case "updateVideo":
+			updateVideo(request, response);
+			break;
+			
+		case "deleteVideo":
+			deleteVideo(request, response);
+			break;
+		}
+		
+	}
+
+
+	private void deleteVideo(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		int id = Integer.parseInt(request.getParameter("videoId"));
+		
+		videoService.del(id);
+		response.sendRedirect("/video?action=selectVideoList");
+		
+	}
+
+
+	private void updateVideo(HttpServletRequest request, HttpServletResponse response) {
+		
+		String title = request.getParameter("title");
+		String url = request.getParameter("url");
+		String part = request.getParameter("part");
+		String channelName = request.getParameter("channelName");
+		
+		Video video = new Video(title, url, part, channelName);
+		videoService.ins(video);
+		
+	}
+
+
+	private void insertVideo(HttpServletRequest request, HttpServletResponse response) {
+		
+		String title = request.getParameter("title");
+		String url = request.getParameter("url");
+		String part = request.getParameter("part");
+		String channelName = request.getParameter("channelName");
+		
+		Video video = new Video(title, url, part, channelName);
+		videoService.ins(video);
+		
+	}
+
+
+	private void selectVideoDetail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int id = Integer.parseInt(request.getParameter("videoId"));
+		
+		videoService.updViewCnt(id);
+		
+		request.setAttribute("video", videoService.selDtl(id));
+		request.getRequestDispatcher("/video/videoDetail.jsp").forward(request, response);
+		
+	}
+
+
+	private void selectVideoList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setAttribute("videos", videoService.selList());
+		request.getRequestDispatcher("/video/videoDetail.jsp").forward(request, response);
 	}
 	
 	
